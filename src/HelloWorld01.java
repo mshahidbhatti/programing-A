b478204649ccd0ba32ba0d35c1d790e13bf07e4d
mshahidbhatti <mshahidbhatti@gmail.com>
2025-02-11 17:07:43 +0500

    first day

---
+++ src/HelloWorld01.java
@@ -0,0 +1,21 @@
+
+class HelloWorld01{
+
+	 public static void main(String[] args){
+
+		System.out.print("Hello World");
+
+		System.out.println("Using println");
+
+		//%s is format specifier for String
+
+		System.out.printf("%03d %-20s\t %12s %.2f\n",1,"Abdul Wahab","FA24-BCS-004",3.5);
+System.out.printf("%03d %-20s\t %12s %.2f\n",2,"Abdul Wahab","FA24-BCS-004",3.5);
+
+System.out.printf("%03d %20s\t %12s %.2f\n",3,"Abdul Wahab","FA24-BCS-004",3.5);
+
+
+	}
+
+
+}
